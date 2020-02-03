87bccc43afe5a385237694e3baf07d3225f0fb18
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-03 15:31:36 -0800

    mock correctly.

--- src/services/__mocks__/futurama-api.js
+++ src/services/__mocks__/futurama-api.js
@@ -1,7 +1,5 @@
-export const quote = () => {
-  return {
-    character:	'Professor Farnsworth',
-    quote:	'I\'m as spry as a 140 year old. See? I only broke one ankle.',
-    image:	'https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/farnsworth.png'
-  };
-};
+export const quote = jest.fn(() => Promise.resolve({
+  character:	'Professor Farnsworth',
+  quote:	'I\'m as spry as a 140 year old. See? I only broke one ankle.',
+  image:	'https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/farnsworth.png'
+}));
