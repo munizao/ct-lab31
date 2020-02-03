060c0d870c16f9993d2395a80f0cfafbe699d04b
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-03 14:41:27 -0800

    mock

---
+++ src/services/__mocks__/futurama-api.js
@@ -0,0 +1,7 @@
+export const quote = () => {
+  return {
+    character:	'Professor Farnsworth',
+    quote:	'I\'m as spry as a 140 year old. See? I only broke one ankle.',
+    image:	'https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/farnsworth.png'
+  };
+};
