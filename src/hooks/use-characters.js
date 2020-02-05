16fdfa443acf46d1a0b3d5a6337aa73f1265f97f
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-04 21:16:40 -0800

    added major characters

--- src/hooks/use-characters.js
+++ src/hooks/use-characters.js
@@ -1,26 +1,46 @@
 import { useState, useEffect } from 'react';
 import { charactersAPI } from '../services/futurama-api';
 
+const majorCharacters = [
+  { name: 'Fry' },
+  { name: 'Bender' },
+  { name: 'Leela' },
+  { name: 'Professor Farnsworth' },
+  { name: 'Amy' },
+  { name: 'Zapp Brannigan' },
+  { name: 'Lurr' },
+  { name: 'Dr Zoidberg' },
+  { name: 'Bob Barker' },
+  { name: 'Hermes' },
+  { name: 'Morgan Proctor' },
+  { name: 'Mom' },
+  { name: 'Robot Mob' },
+  { name: 'Giant Bender' },
+  { name: 'Kif' }
+];
+
 const useCharacters = () => {
   const [characters, setCharacters] = useState([]);
-  const [, setCharacterId] = useState(0);
+  const [selectedCharacterIndex, setSelectedCharacterIndex] = useState(-1);
+  const [charactersAreMajor, setCharactersAreMajor] = useState(true);
 
   const fetchCharacters = () => {
-    return charactersAPI()
-      .then((res) => {
-        setCharacters(res);
-      });  
+    if(charactersAreMajor) {
+      setCharacters(majorCharacters);
+    }
+    else {
+      charactersAPI()
+        .then((res) => {
+          setCharacters(res);
+        });  
+    }
   };
 
-  const changeCharacter = ({ target }) => {
-    setCharacterId(target.value);
-  };
-  
   useEffect(() => {
     fetchCharacters();    
-  }, []);
+  }, [charactersAreMajor]);
 
-  return { characters, changeCharacter };
+  return { characters, selectedCharacterIndex, setSelectedCharacterIndex, setCharactersAreMajor };
 };
 
 export default useCharacters;
