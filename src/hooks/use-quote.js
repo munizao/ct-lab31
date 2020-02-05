e9ce8a7a24d4711960450dd1292005f8e45950fe
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-04 21:16:22 -0800

    useQuote takes characters

--- src/hooks/use-quote.js
+++ src/hooks/use-quote.js
@@ -1,18 +1,23 @@
 import { quoteAPI } from '../services/futurama-api';
 
 import { useState, useEffect } from 'react';
-const useQuote = () => {
+const useQuote = (characters, index) => {
   const [quoteObj, setQuoteObj] = useState({ quote: '', image: '', character: '' });
   const fetchQuote = () => {
-    return quoteAPI()
+    return quoteAPI(characters.length > 0 ? characters[index].name : null)
       .then((res) => {
-        setQuoteObj(res);
+        if(res) {
+          setQuoteObj(res);
+        }
+        else {
+          setQuoteObj({ quote: 'No Quote Found', image: '', character: '' });
+        }
       });  
   };
   
   useEffect(() => {
     fetchQuote();    
-  }, []);
+  }, [index]);
 
   return { quoteObj, fetchQuote } ;
 };
